25faeb4783b5f3af4214bd7eec95102cd32d1f86
Joshua Cano <joshuacano@Joshuas-MBP.attlocal.net>
2024-09-12 12:19:09 -0500

    Updated Logos and banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/preline/preline.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        fluid: "clamp(0.55rem, 3vw, 0.8rem)",
+      },
+    },
   },
   plugins: [require("preline/plugin")],
 };
